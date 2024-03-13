2cdd01708038acae4d3c4d176b54cd1f51dbac75
sasa_takayuki <162660897+sasa-takayuki@users.noreply.github.com>
2024-03-13 16:50:28 +0900

    Update bmi.js

--- bmi.js
+++ bmi.js
@@ -1,5 +1,5 @@
 // 変数の宣言
-let bmi;s
+let bmi;
 // 体重と身長のBMI（体格指数）
 // 体重：68kg
 weight = 68
@@ -9,5 +9,5 @@ height = 1.7
 BMI =bmi
 
 // 計算式：[体重（kg）÷[身長（m）×身長（m）]のコンソールへの出力
-console.log(weight / (height * height)) 
+console.log(68 / (1.7 * 1.7)) ;
    
