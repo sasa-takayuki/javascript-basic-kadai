ac8c8c8c6e898758d19e1cb9f0d09aad50dd3581
sasa_takayuki <162660897+sasa-takayuki@users.noreply.github.com>
2024-03-13 21:05:47 +0900

    Update bmi.js

--- bmi.js
+++ bmi.js
@@ -5,7 +5,7 @@ weight = 68
 // 身長：1.7m
 height = 1.7
 // 計算式：[体重（kg）÷[身長（m）×身長（m）]
-BMI = console.log(weight / (height * height));
+BMI =(weight / (height * height));
 // コンソールへの出力
 console.log('BMI');
 
